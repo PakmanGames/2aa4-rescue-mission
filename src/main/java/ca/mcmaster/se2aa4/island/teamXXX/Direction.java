db08e4e02f3bc6e5c86926626964693fa3cb0def
Andy Pak <im.andy.pak@gmail.com>
2025-03-08 14:33:08 -0500

    Create Direction enum closes #8

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Direction.java
@@ -0,0 +1,39 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public enum Direction {
+    NORTH("N"),
+    EAST("E"),
+    SOUTH("S"),
+    WEST("W");
+
+    private final String abbr;
+    private Direction left;
+    private Direction right;
+
+    Direction(String abbr) {
+        this.abbr = abbr;
+    }
+
+    static {
+        NORTH.left = WEST;
+        NORTH.right = EAST;
+        EAST.left = NORTH;
+        EAST.right = SOUTH;
+        SOUTH.left = EAST;
+        SOUTH.right = WEST;
+        WEST.left = SOUTH;
+        WEST.right = NORTH;
+    }
+
+    public String getAbbr() {
+        return abbr;
+    }
+
+    public Direction getLeft() {
+        return left;
+    }
+
+    public Direction getRight() {
+        return right;
+    }
+}
