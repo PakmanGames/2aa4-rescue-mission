4ed72f14edbcf6364b089bb84988daff2e1c548e
Sixbysix616 <113481122+Sixbysix616@users.noreply.github.com>
2025-03-10 21:06:27 -0400

    Create initialize and takeDecision method test class

---
+++ src/test/java/ca/mcmaster/se2aa4/island/teamXXX/TakeDecisionTest.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+public class TakeDecisionTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(1 == 1);
+    }
+
+
+}
