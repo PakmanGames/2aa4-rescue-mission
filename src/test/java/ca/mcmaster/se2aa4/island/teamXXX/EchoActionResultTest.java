00b63902764aa5c2642941023584d39a85a279c9
Andy Pak <im.andy.pak@gmail.com>
2025-03-17 13:47:31 -0400

    Write unit tests for EchoActionResult

---
+++ src/test/java/ca/mcmaster/se2aa4/island/teamXXX/EchoActionResultTest.java
@@ -0,0 +1,24 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ca.mcmaster.se2aa4.island.teamXXX.result.EchoActionResult;
+
+public class EchoActionResultTest {
+    @Test
+    public void testEchoActionResult() {
+        EchoActionResult result = new EchoActionResult(5, true);
+        assertEquals(5, result.range());
+        assertTrue(result.foundGround());
+        assertEquals("EchoActionResult[range=5, found=GROUND]", result.toString());
+    }
+
+    @Test
+    public void testEchoActionResultOutOfRange() {
+        EchoActionResult result = new EchoActionResult(0, false);
+        assertEquals(0, result.range());
+        assertFalse(result.foundGround());
+        assertEquals("EchoActionResult[range=0, found=OUT_OF_RANGE]", result.toString());
+    }
+}
