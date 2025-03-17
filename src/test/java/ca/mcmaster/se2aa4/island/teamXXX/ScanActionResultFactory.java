a92a85b7a5180fa8d9011e1bae98a6abb3e0682c
Andy Pak <im.andy.pak@gmail.com>
2025-03-17 14:08:45 -0400

    Create test cases for ScanActionResult

---
+++ src/test/java/ca/mcmaster/se2aa4/island/teamXXX/ScanActionResultFactory.java
@@ -0,0 +1,35 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import java.util.List;
+import java.util.Arrays;
+
+import ca.mcmaster.se2aa4.island.teamXXX.result.ScanActionResult;
+
+public class ScanActionResultFactory {
+    public List createCreeks() {
+        return Arrays.asList("creek_id1", "creek_id2");
+    }
+
+    public List createSites() {
+        return Arrays.asList("site_id1", "site_id2");
+    }
+
+    public ScanActionResult createScanActionResult() {
+        List<String> creeks = this.createCreeks();
+        List<String> sites = this.createSites();
+
+        return new ScanActionResult(creeks, sites);
+    }
+
+    public ScanActionResult createScanActionResultWithNullCreeks() {
+        List<String> sites = this.createSites();
+
+        return new ScanActionResult(null, sites);
+    }
+
+    public ScanActionResult createScanActionResultWithNullSites() {
+        List<String> creeks = this.createCreeks();
+
+        return new ScanActionResult(creeks, null);
+    }
+}
