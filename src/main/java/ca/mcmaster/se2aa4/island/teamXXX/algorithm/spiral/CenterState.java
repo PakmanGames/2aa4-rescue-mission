6ab3a1af8913ad2c8b8af4a753f48a7ae49c2e2c
Sixbysix616 <113481122+Sixbysix616@users.noreply.github.com>
2025-03-21 17:24:05 -0400

    isCenter()can have 1 unit error now. Try to do getAction().

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/algorithm/spiral/CenterState.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/algorithm/spiral/CenterState.java
@@ -2,47 +2,60 @@ package ca.mcmaster.se2aa4.island.teamXXX.algorithm.spiral;
 
 import ca.mcmaster.se2aa4.island.teamXXX.actions.Action;
 import ca.mcmaster.se2aa4.island.teamXXX.algorithm.State;
+import ca.mcmaster.se2aa4.island.teamXXX.drone.Direction;
 import ca.mcmaster.se2aa4.island.teamXXX.drone.Drone;
-import ca.mcmaster.se2aa4.island.teamXXX.drone.Position;
 import ca.mcmaster.se2aa4.island.teamXXX.result.ActionResult;
 
 public class CenterState extends State {
-    private int mapWidth;
-    private int mapHeight;
+    private final int centerX;
+    private final int centerY;
+    private Action action;
 
-    public CenterState(Drone drone,int mapHeight,int mapWidth) {//Assume DimensionFindingState can transfer width and height as parameter
+    public CenterState(Drone drone, int mapHeight, int mapWidth) {
         super(drone);
-        this.mapHeight = mapHeight;
-        this.mapWidth = mapWidth;
+        this.centerX = (int) Math.ceil(mapWidth / 2.0);
+        this.centerY = (int) Math.ceil(mapHeight / 2.0);
     }
 
-    public boolean isCenter(Position position){
-        int centerX = (int) Math.ceil(mapWidth / 2);
-        int centerY = (int) Math.ceil(mapHeight / 2);
-        boolean is_center = (position.getX()== centerX && position.getY() == centerY);
-        return is_center;
+    public boolean isCenter() {
+        int currentX = getDrone().getPosition().getX();
+        int currentY = getDrone().getPosition().getY();
+        int deltaX = centerX - currentX;
+        int deltaY = centerY - currentY;
 
+        return Math.abs(deltaX) <= 1 && Math.abs(deltaY) <= 1;
     }
 
     @Override
-    public State nextState(ActionResult action) {
-        // If the state still has work to be done then it should go to the current state
-        // Otherwise it should go to the SpiralState
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'nextState'");
-
-        if (isCenter(getDrone().getPosition())) {
-            return new SpiralState(getDrone());
-        }
-
-        return this;//if not center, stay at current state
+    public State nextState(ActionResult result) {
+        Drone drone = getDrone();
+        drone.expend(result.getCost());
+        action.consume(drone, result);
+        return isCenter() ? new SpiralState(getDrone()) : this;
     }
-
 
     @Override
     public Action getAction() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAction'");
-    }
+        Drone drone = getDrone();
+        int currentX = drone.getPosition().getX();
+        int currentY = drone.getPosition().getY();
 
+        int deltaX = centerX - currentX;
+        int deltaY = centerY - currentY;
+        
+        Direction currentDirection = drone.getDirection();
+
+        if (isCenter()) {
+            return null; 
+        }
+
+
+
+
+
+   
+       action = drone.fly();
+
+       return action;
+    }
 }
