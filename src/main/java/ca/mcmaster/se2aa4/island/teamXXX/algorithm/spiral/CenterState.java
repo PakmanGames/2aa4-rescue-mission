7854f08d46c5ac176338864094a57088a764f427
Sixbysix616 <113481122+Sixbysix616@users.noreply.github.com>
2025-03-21 18:21:56 -0400

    Finish getAction() and the error is 2units now.

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/algorithm/spiral/CenterState.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/algorithm/spiral/CenterState.java
@@ -7,8 +7,8 @@ import ca.mcmaster.se2aa4.island.teamXXX.drone.Drone;
 import ca.mcmaster.se2aa4.island.teamXXX.result.ActionResult;
 
 public class CenterState extends State {
-    private final int centerX;
-    private final int centerY;
+    private int centerX;
+    private int centerY;
     private Action action;
 
     public CenterState(Drone drone, int mapHeight, int mapWidth) {
@@ -23,7 +23,7 @@ public class CenterState extends State {
         int deltaX = centerX - currentX;
         int deltaY = centerY - currentY;
 
-        return Math.abs(deltaX) <= 1 && Math.abs(deltaY) <= 1;
+        return Math.abs(deltaX) <= 2 && Math.abs(deltaY) <= 2;
     }
 
     @Override
@@ -45,16 +45,106 @@ public class CenterState extends State {
         
         Direction currentDirection = drone.getDirection();
 
-        if (isCenter()) {
-            return null; 
+        if(Math.abs(deltaX)>2 && Math.abs(deltaY)>2){
+            if(deltaX>0 &&deltaY>0){{//drone is at the lower leftcorner 
+                if (currentDirection ==Direction.NORTH ||currentDirection ==Direction.WEST){
+                    action = drone.head(currentDirection.right());
+                }
+                else if (currentDirection ==Direction.SOUTH||currentDirection ==Direction.EAST){
+                    action = drone.head(currentDirection.left());
+
+
+            }
+            else if (deltaX<0 && deltaY>0)//drone is at the lower right corener
+                if (currentDirection ==Direction.SOUTH ||currentDirection ==Direction.WEST){
+                    action = drone.head(currentDirection.right());
+                }
+                else if (currentDirection ==Direction.NORTH||currentDirection ==Direction.EAST){
+                    action = drone.head(currentDirection.left());
+
+            }
+
+            else if  (deltaX>0 && deltaY<0){//drone is at the lower right corner 
+                if (currentDirection ==Direction.EAST ||currentDirection ==Direction.NORTH){
+                    action = drone.head(currentDirection.right());
+                }
+                else if (currentDirection ==Direction.SOUTH ||currentDirection ==Direction.WEST){
+                    action = drone.head(currentDirection.left());
+                }
+
+            }
+
+            else{//drone is at the upper right corner
+                if (currentDirection ==Direction.SOUTH ||currentDirection ==Direction.EAST){
+                    action = drone.head(currentDirection.right());
+                }
+                else if (currentDirection ==Direction.NORTH ||currentDirection ==Direction.WEST){
+                    action = drone.head(currentDirection.left());
+
+            }
+
         }
+        if(deltaX<=2){
+            if (deltaY>0){//drone is south to center
+               if (currentDirection == Direction.NORTH){
+                action = drone.fly();
+               }
+               else {//The direction must be WEST or EAST
+                    if (currentDirection == Direction.EAST){
+                        action = drone.head(currentDirection.left());
+                    }
+                    else {
+                        action = drone.head(currentDirection.right())
+                    }
+                }
+              
+            }
+            else{//drone is NORTH to the center
+                if (currentDirection == Direction.SOUTH){
+                action = drone.fly();
+               }
+               else {//The direction must be WEST or EAST
+                    if (currentDirection == Direction.EAST){
+                        action = drone.head(currentDirection.right());
+                    }
+                    else {
+                        action = drone.head(currentDirection.left())
+                    }
+                }
 
+            }
+        
 
+        }
+        else if (deltaY<=2){
+            if (deltaX>0){//drone is WEST to center
+               if (currentDirection == Direction.EAST){
+                action = drone.fly();
+               }
+               else {//The direction must be NORTH or SOUTH
+                    if (currentDirection == Direction.SOUTH){
+                        action = drone.head(currentDirection.left());
+                    }
+                    else {
+                        action = drone.head(currentDirection.right())
+                    }
+                }
+              
+            }
+            else{//drone is EAST to the center
+                if (currentDirection == Direction.NORTH){
+                action = drone.fly();
+               }
+               else {//The direction must be NORTH or SOUTH
+                    if (currentDirection == Direction.SOUTH){
+                        action = drone.head(currentDirection.right());
+                    }
+                    else {
+                        action = drone.head(currentDirection.left())
+                    }
+                }
 
-
-
-   
-       action = drone.fly();
+        }
 
        return action;
     }
